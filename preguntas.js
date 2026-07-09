5a04c9c24bd58956b2c3e92f66a3725c1c1034ba
wolfanjohanny-afk <wolfanjohanny@gmail.com>
2026-07-09 17:40:21 -0500

    Create preguntas.js

---
+++ preguntas.js
@@ -0,0 +1,12 @@
+const preguntas = [
+  {
+    texto: "Pregunta de prueba",
+    opciones: [
+      "Totalmente de acuerdo",
+      "De acuerdo",
+      "Neutral",
+      "En desacuerdo",
+      "Totalmente en desacuerdo"
+    ]
+  }
+];
