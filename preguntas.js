13b410c5dd1433713ed02c2f585a774dcdf7323d
wolfanjohanny-afk <wolfanjohanny@gmail.com>
2026-07-09 18:21:02 -0500

    Update preguntas.js

--- preguntas.js
+++ preguntas.js
@@ -1,24 +1,36 @@
 const preguntas = [
-  {
-    id: 1,
-    texto: "Me siento tranquilo cuando ahorro dinero.",
-    opciones: [
-      "Totalmente de acuerdo",
-      "De acuerdo",
-      "Neutral",
-      "En desacuerdo",
-      "Totalmente en desacuerdo"
-    ]
-  },
-  {
-    id: 2,
-    texto: "Antes de comprar algo importante, lo pienso varias veces.",
-    opciones: [
-      "Totalmente de acuerdo",
-      "De acuerdo",
-      "Neutral",
-      "En desacuerdo",
-      "Totalmente en desacuerdo"
-    ]
-  }
+
+{
+texto: "Cuando recibes dinero extra, normalmente...",
+opciones:[
+"Lo ahorro inmediatamente",
+"Invierto una parte",
+"Lo gasto en algo que deseo",
+"Lo comparto con otros",
+"Improviso sin pensarlo"
+]
+},
+
+{
+texto:"Antes de comprar algo costoso...",
+opciones:[
+"Lo pienso varios días",
+"Comparo opciones",
+"Compro por emoción",
+"Pido opinión",
+"Lo compro inmediatamente"
+]
+},
+
+{
+texto:"Tu principal meta financiera es...",
+opciones:[
+"Tener seguridad",
+"Hacer crecer mi patrimonio",
+"Disfrutar la vida",
+"Ayudar a otros",
+"Tener libertad"
+]
+},
+
 ];
