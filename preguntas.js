34009c4438524ef50e0afe34b8ca491236a13a01
wolfanjohanny-afk <wolfanjohanny@gmail.com>
2026-07-09 18:04:32 -0500

    Update preguntas.js

--- preguntas.js
+++ preguntas.js
@@ -1,6 +1,18 @@
 const preguntas = [
   {
-    texto: "Pregunta de prueba",
+    id: 1,
+    texto: "Me siento tranquilo cuando ahorro dinero.",
+    opciones: [
+      "Totalmente de acuerdo",
+      "De acuerdo",
+      "Neutral",
+      "En desacuerdo",
+      "Totalmente en desacuerdo"
+    ]
+  },
+  {
+    id: 2,
+    texto: "Antes de comprar algo importante, lo pienso varias veces.",
     opciones: [
       "Totalmente de acuerdo",
       "De acuerdo",
