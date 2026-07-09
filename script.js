9e78e23ddcd368d9e02be35942464a6c7af2a05d
wolfanjohanny-afk <wolfanjohanny@gmail.com>
2026-07-09 18:15:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 let indicePregunta = 0;
+let respuestas = [];
 
 function iniciarTest() {
-    console.log("Test iniciado");
+    console.log("Sistema iniciado");
+}
+
+function mostrarPregunta() {
+    console.log("Mostrando pregunta", indicePregunta);
+}
+
+function responder(opcion) {
+    respuestas.push(opcion);
+    indicePregunta++;
+    mostrarPregunta();
 }
 
 window.onload = iniciarTest;
